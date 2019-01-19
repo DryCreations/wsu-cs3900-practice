d28d674a7140b65ebd2e73192f369159d2a39186
Hayden Mankin <DryCreations@gmail.com>
2019-01-18 20:11:11 -0500

    Updates

---
+++ precache-manifest.bc697ca74ed364e0ca7ff9d5f7304b5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "71606865a4ba8ac054c1",
+    "url": "/wsu-cs3900-practice/static/js/runtime~main.71606865.js"
+  },
+  {
+    "revision": "7d87a3a802f2655fc9b5",
+    "url": "/wsu-cs3900-practice/static/js/main.7d87a3a8.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/wsu-cs3900-practice/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "7d87a3a802f2655fc9b5",
+    "url": "/wsu-cs3900-practice/static/css/main.42ce08b7.chunk.css"
+  },
+  {
+    "revision": "4a31aec484d7995eef49dd7303a88d63",
+    "url": "/wsu-cs3900-practice/index.html"
+  }
+];
